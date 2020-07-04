335ae3ca24d0e4ecf305ef26982134f55ea286f2
Avery Crespi <averycrespi@gmail.com>
2020-07-04 08:16:10 -0700

    Refactor room to match and remove schema

---
+++ src/api/match.test.ts
@@ -0,0 +1,89 @@
+import { Player, opponentOf } from "../logic";
+import {
+  createMatch,
+  getMatch,
+  joinMatch,
+  leaveMatch,
+  listMatchIDs,
+} from "./match";
+
+import { Server } from "boardgame.io/server";
+import { game } from "../logic/game";
+
+const port = 8000;
+const serverURL = `http://localhost:${port}`;
+
+let commands: { run: any; kill: any };
+let server: { apiServer: any; appServer: any };
+beforeAll(async () => {
+  commands = Server({ games: [game] });
+  server = await commands.run({ port });
+});
+
+afterAll(() => commands.kill(server));
+
+test("list is empty", async () => {
+  expect(await listMatchIDs(serverURL)).toStrictEqual([]);
+});
+
+let matchID: string;
+test("create match", async () => {
+  matchID = await createMatch(serverURL);
+});
+
+test("list contains match", async () => {
+  const matchIDs = await listMatchIDs(serverURL);
+  expect(matchIDs).toStrictEqual([matchID]);
+});
+
+const player = Player.Red;
+const playerName = "Alice";
+let playerCredentials: string;
+test("player joins match ", async () => {
+  playerCredentials = await joinMatch(serverURL, matchID, player, playerName);
+});
+
+test("player in match", async () => {
+  const match = await getMatch(serverURL, matchID);
+  expect(match.matchID).toBe(matchID);
+  expect(match.players.get(player)).toBe(playerName);
+  expect(match.players.get(opponentOf(player))).toBe(null);
+});
+
+const opponent = opponentOf(player);
+const opponentName = "Bob";
+let opponentCredentials: string;
+test("opponent joins match", async () => {
+  opponentCredentials = await joinMatch(
+    serverURL,
+    matchID,
+    opponent,
+    opponentName
+  );
+});
+
+test("player and opponent in match", async () => {
+  const match = await getMatch(serverURL, matchID);
+  expect(match.matchID).toBe(matchID);
+  expect(match.players.get(player)).toBe(playerName);
+  expect(match.players.get(opponentOf(player))).toBe(opponentName);
+});
+
+test("player leaves match", async () => {
+  await leaveMatch(serverURL, matchID, player, playerCredentials);
+});
+
+test("opponent in match", async () => {
+  const match = await getMatch(serverURL, matchID);
+  expect(match.matchID).toBe(matchID);
+  expect(match.players.get(player)).toBe(null);
+  expect(match.players.get(opponentOf(player))).toBe(opponentName);
+});
+
+test("opponent leaves match", async () => {
+  await leaveMatch(serverURL, matchID, opponent, opponentCredentials);
+});
+
+test("list is empty", async () => {
+  expect(await listMatchIDs(serverURL)).toStrictEqual([]);
+});
---
+++ src/api/match.ts
@@ -0,0 +1,132 @@
+/**
+ * This module follows the API schema from: https://boardgame.io/documentation/#/api/Lobby
+ * v0.40.0 will cause breaking changes: https://github.com/boardgameio/boardgame.io/pull/709
+ *
+ * Known inconsistencies:
+ *  - matchID is called gameID or roomID, depending on the endpoint.
+ *  - playerID is treated as a number, not a string.
+ */
+
+import { Player } from "../logic";
+import { game } from "../logic/game";
+
+const headers = {
+  Accept: "application/json",
+  "Content-Type": "application/json",
+};
+
+/** Represents a match. */
+export interface Match {
+  matchID: string;
+  players: Map<Player, string | null>;
+}
+
+/** Create a match and return the match ID. */
+export const createMatch = async (
+  serverURL: string,
+  unlisted: boolean = false
+): Promise<string> => {
+  const resp = await fetch(`${serverURL}/games/${game.name}/create`, {
+    method: "POST",
+    headers,
+    body: JSON.stringify({ numPlayers: 2, unlisted }),
+  });
+  if (!resp.ok) {
+    throw new Error("failed to create match: " + (await resp.text()));
+  }
+  const data = await resp.json();
+  if (typeof data.gameID !== "string") {
+    throw new Error("invalid response data: " + JSON.stringify(data));
+  } else {
+    return data.gameID as string;
+  }
+};
+
+/** Join a match and return the player credentials. */
+export const joinMatch = async (
+  serverURL: string,
+  matchID: string,
+  player: Player,
+  name: string
+): Promise<string> => {
+  const resp = await fetch(`${serverURL}/games/${game.name}/${matchID}/join`, {
+    method: "POST",
+    headers,
+    body: JSON.stringify({ playerID: player, playerName: name }),
+  });
+  if (!resp.ok) {
+    throw new Error("failed to join match: " + (await resp.text()));
+  }
+  const data = await resp.json();
+  if (typeof data.playerCredentials !== "string") {
+    throw new Error("invalid response data: " + JSON.stringify(data));
+  } else {
+    return data.playerCredentials as string;
+  }
+};
+
+/** Leave a match. */
+export const leaveMatch = async (
+  serverURL: string,
+  matchID: string,
+  player: Player,
+  credentials: string
+): Promise<void> => {
+  const resp = await fetch(`${serverURL}/games/${game.name}/${matchID}/leave`, {
+    method: "POST",
+    headers,
+    body: JSON.stringify({ playerID: player, credentials }),
+  });
+  if (!resp.ok) {
+    throw new Error("failed to leave match: " + (await resp.text()));
+  }
+};
+
+/** List the available match IDs. */
+export const listMatchIDs = async (
+  serverURL: string
+): Promise<Array<string>> => {
+  const resp = await fetch(`${serverURL}/games/${game.name}`);
+  if (!resp.ok) {
+    throw new Error("failed to list match IDs: " + (await resp.text()));
+  }
+  const data = await resp.json();
+  if (
+    !Array.isArray(data.rooms) ||
+    data.rooms.some((r: any) => typeof r.gameID !== "string")
+  ) {
+    throw new Error("invalid response data: " + JSON.stringify(data));
+  } else {
+    return data.rooms.map((r: any) => r.gameID) as Array<string>;
+  }
+};
+
+/** Get information about a match. */
+export const getMatch = async (
+  serverURL: string,
+  matchID: string
+): Promise<Match> => {
+  const resp = await fetch(`${serverURL}/games/${game.name}/${matchID}`);
+  if (!resp.ok) {
+    throw new Error("failed to get match: " + (await resp.text()));
+  }
+  const data = await resp.json();
+  if (
+    typeof data.roomID !== "string" ||
+    !Array.isArray(data.players) ||
+    data.players.some((p: any) => typeof p.id !== "number") ||
+    data.players.some((p: any) => p.name && typeof p.name !== "string")
+  ) {
+    throw new Error("invalid response data: " + JSON.stringify(data));
+  } else {
+    return {
+      matchID: data.roomID as string,
+      players: new Map<Player, string | null>(
+        data.players.map((p: any) => [
+          p.id.toString() as Player,
+          p.name ?? null,
+        ])
+      ),
+    };
+  }
+};
