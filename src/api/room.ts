75f7e6c608b5a2b98ddba2e2f3b8fcc237cae948
Avery Crespi <averycrespi@gmail.com>
2020-07-03 17:32:50 -0700

    Add room API tests

--- src/api/room.ts
+++ src/api/room.ts
@@ -1,14 +1,22 @@
-/** This module follows the API schema from: https://boardgame.io/documentation/#/api/Lobby */
+/**
+ * This module follows the API schema from: https://boardgame.io/documentation/#/api/Lobby
+ * 0.40.0 will cause breaking changes: https://github.com/boardgameio/boardgame.io/pull/709
+ */
 
 import { game } from "../logic/game";
 import { validate } from "jsonschema";
 
 const roomSchema = require("./room.json");
 
+const headers = {
+  Accept: "application/json",
+  "Content-Type": "application/json",
+};
+
 /** Represents a game room. */
 export interface Room {
-  roomID: string;
-  players: Array<{ id: string; name?: string }>;
+  gameID: string;
+  players: Array<{ id: 0 | 1; name?: string }>;
   setupData?: any;
 }
 
@@ -20,12 +28,14 @@ export const createRoom = async (
 ): Promise<string> => {
   const resp = await fetch(`${serverURL}/games/${game.name}/create`, {
     method: "POST",
+    headers,
     body: JSON.stringify({ numPlayers: 2, unlisted, setupData }),
   });
-  const data = await resp.json();
   if (!resp.ok) {
-    throw new Error("failed to create room: " + JSON.stringify(data));
-  } else if (typeof data.gameID !== "string") {
+    throw new Error("failed to create room" + (await resp.text()));
+  }
+  const data = await resp.json();
+  if (typeof data.gameID !== "string") {
     throw new Error("invalid response data: " + JSON.stringify(data));
   } else {
     return data.gameID as string;
@@ -35,19 +45,21 @@ export const createRoom = async (
 /** Join a room and return the player credentials. */
 export const joinRoom = async (
   serverURL: string,
-  roomID: string,
-  playerID: string,
+  gameID: string,
+  playerID: 0 | 1,
   playerName: string,
   playerData: any = {}
 ): Promise<string> => {
-  const resp = await fetch(`${serverURL}/games/${game.name}/${roomID}/join`, {
+  const resp = await fetch(`${serverURL}/games/${game.name}/${gameID}/join`, {
     method: "POST",
+    headers,
     body: JSON.stringify({ playerID, playerName, data: playerData }),
   });
-  const data = await resp.json();
   if (!resp.ok) {
-    throw new Error("failed to join room: " + JSON.stringify(data));
-  } else if (typeof data.playerCredentials !== "string") {
+    throw new Error("failed to join room: " + (await resp.text()));
+  }
+  const data = await resp.json();
+  if (typeof data.playerCredentials !== "string") {
     throw new Error("invalid response data: " + JSON.stringify(data));
   } else {
     return data.playerCredentials as string;
@@ -57,29 +69,30 @@ export const joinRoom = async (
 /** Leave a room. */
 export const leaveRoom = async (
   serverURL: string,
-  roomID: string,
-  playerID: string,
+  gameID: string,
+  playerID: 0 | 1,
   credentials: string
 ): Promise<void> => {
-  const resp = await fetch(`${serverURL}/games/${game.name}/${roomID}/leave`, {
+  const resp = await fetch(`${serverURL}/games/${game.name}/${gameID}/leave`, {
     method: "POST",
+    headers,
     body: JSON.stringify({ playerID, credentials }),
   });
-  const data = await resp.json();
   if (!resp.ok) {
-    throw new Error("failed to leave room: " + JSON.stringify(data));
+    throw new Error("failed to leave room: " + (await resp.text()));
   }
 };
 
 /** List the available rooms. */
 export const listRooms = async (serverURL: string): Promise<Array<Room>> => {
   const resp = await fetch(`${serverURL}/games/${game.name}`);
-  const data = await resp.json();
   if (!resp.ok) {
-    throw new Error("failed to list rooms: " + JSON.stringify(data));
-  } else if (
+    throw new Error("failed to list rooms: " + (await resp.text()));
+  }
+  const data = await resp.json();
+  if (
     !Array.isArray(data.rooms) ||
-    data.rooms.some((r: any) => !validate(r, roomSchema))
+    data.rooms.some((r: any) => !validate(r, roomSchema).valid)
   ) {
     throw new Error("invalid response data: " + JSON.stringify(data));
   } else {
@@ -87,16 +100,18 @@ export const listRooms = async (serverURL: string): Promise<Array<Room>> => {
   }
 };
 
-/** Get information about a specific room. */
+/** Get information about a room. */
 export const getRoom = async (
   serverURL: string,
-  roomID: string
+  gameID: string
 ): Promise<Room> => {
-  const resp = await fetch(`${serverURL}/games/${game.name}/${roomID}`);
-  const data = await resp.json();
+  const resp = await fetch(`${serverURL}/games/${game.name}/${gameID}`);
   if (!resp.ok) {
-    throw new Error("failed to get room: " + JSON.stringify(data));
-  } else if (!validate(data, roomSchema)) {
+    throw new Error("failed to get room: " + (await resp.text()));
+  }
+  const data = await resp.json();
+  data.gameID = data.roomID; // Fix API inconsistency.
+  if (!validate(data, roomSchema).valid) {
     throw new Error("invalid response data: " + JSON.stringify(data));
   } else {
     return data as Room;
